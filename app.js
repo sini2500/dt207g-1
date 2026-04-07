be8cf6e12d0fcd039a017f47faa707d4f1a9c7d2
Simon N. <sini2500@student.miun.se>
2026-04-07 23:24:00 +0200

    port?

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Statiskt
 app.use(express.static("public"));
